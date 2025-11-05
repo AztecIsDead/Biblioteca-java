ae819ba130ae36d70a31f49dc17a5b28efb19249
Caius Vinícius Araújo de Farias <caius.farias@somosicev.com>
2025-11-04 22:45:27 -0300

    Uq eu quis dizer foi Remoção do int páginas
    só corrigindo-

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Funcionario Caius = new Funcionario("Caius", 20, "Cargo" );
         System.out.println(Caius);
+        //só ajeitando o commit
     }
 }
